aa959993ab0360a99e4b2db99285cd1448d2d000
Alexey <ihimself@gmail.com>
2023-03-04 11:51:58 +0100

    removed not used code from db, adjusted text in index

--- src/db/dtu_db.js
+++ src/db/dtu_db.js
@@ -47,29 +47,8 @@ function DB_SELECT_EMULATION_check_if_report_date_matches_dates_in_user_filters(
   return true;
 }
 
-function DB_SELECT_EMULATION_check_if_report_uid_matches_uids_in_user_filters(r, user_filters) {
+function DB_SELECT_EMULATION_check_if_report_ctag_topic_dates_match_user_filters(r, user_filters) {
   debug_helper(arguments, DEBUG);
-  let uids = user_filters['uids'];
-  let uids_exclude = user_filters['uids_exclude'];
-
-  if (uids) {
-    if (!uids.includes(String(r.uid))) {
-      return false;
-    }
-  }
-  if (uids_exclude) {
-    if (uids_exclude.includes(String(r.uid))) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-function DB_SELECT_EMULATION_check_if_report_ctag_topic_users_dates_match_user_filters(r, user_filters) {
-  debug_helper(arguments, DEBUG);
-  if (!DB_SELECT_EMULATION_check_if_report_uid_matches_uids_in_user_filters(r, user_filters))
-    return false;
 
   if (!DB_SELECT_EMULATION_check_if_report_ctag_and_topic_match_them_in_user_filters(r, user_filters))
     return false;
@@ -84,18 +63,15 @@ function DB_SELECT_all_WHERE_user_filters(user_filters) {
   // SELECT * FROM reports_table WHERE 1=1
   // AND ctag = user_filters.ctag
   // AND topic = user_filters.topic
-  // AND uid IN user_filters.uids_to_include
-  // AND uid NOT IN user_filters.uids_to_exclude
   // AND date_time BETWEEN (user_filters.date_time_from, user_filters.date_time_to)
-  // AND FOR each_custom_tag DO each_custom_tag = user_filters.custom_tag;
   debug_helper(arguments, DEBUG);
 
-  var found = [];
-  for (let i = 0; i < db.length; i++) {
-    var r = db[i];
-    if (!DB_SELECT_EMULATION_check_if_report_ctag_topic_users_dates_match_user_filters(r, user_filters))
+  let found_reports = [];
+  for (let i in db) {
+    let report = db[i];
+    if (!DB_SELECT_EMULATION_check_if_report_ctag_topic_dates_match_user_filters(report, user_filters))
       continue;
-    found.push(r);
+    found_reports.push(report);
   }
-  return found;
+  return found_reports;
 }
