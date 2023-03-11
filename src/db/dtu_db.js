d7a5b7065297f8a0b216d90a53aa57b8b789a9e4
Alexey <ihimself@gmail.com>
2023-03-11 11:37:24 +0100

    real usage db in session storage but sloooow

--- src/db/dtu_db.js
+++ src/db/dtu_db.js
@@ -1,17 +1,92 @@
-var db = [];
-var db_elements = [];
+class DB {
+  constructor() {
+    this.db_m = {'table_reports': [], 'table_elements': {}};
+    this.init_session_storage();
+  }
 
-// INIT DB DATA:
-db2 = [
+  init_session_storage() {
+    if (!window.sessionStorage.getItem('db')) {
+      window.sessionStorage.setItem('db', JSON.stringify(this.db_m));
+    }
+  }
 
-];
+  read_session_storage() {
+    const db_s = window.sessionStorage.getItem('db');
+    return JSON.parse(db_s);
+  }
 
-function DB_SELECT_all_FROM_db() {  // debug func
-  debug_helper(arguments, DEBUG);
-  for(let i in db) {
-    console.log(JSON.stringify(db[i]));
+  get_type_of_storage(topic) {
+    let type_of_storage = 'session';
+    if (topic != 'real usage')
+      type_of_storage = 'in-memory';
+    return type_of_storage;
+  }
+
+  select(table_name, topic) {
+    const type_of_storage = this.get_type_of_storage(topic);
+
+    if (table_name == 'table_reports') {
+      if (type_of_storage == 'in-memory')
+        return this.db_m.table_reports;
+      else
+        return this.read_session_storage().table_reports;
+    }
+    else if (table_name == 'table_elements') {
+      if (type_of_storage == 'in-memory')
+        return this.db_m.table_elements;
+      else
+        return this.read_session_storage().table_elements;
+    }
+  }
+
+  insert(records, table_name, topic) {
+    const type_of_storage = this.get_type_of_storage(topic);
+    //const existing_records = this.select(table_name, type_of_storage);
+    //const new_records = existing_records.concat(records);
+
+    if (table_name == 'table_reports') {
+      if (type_of_storage == 'in-memory') {
+        let existing_records = this.select(table_name, type_of_storage);
+        this.db_m.table_reports = existing_records.concat(records);
+      }
+      else {
+        let db_s_json = this.read_session_storage();
+        db_s_json.table_reports = db_s_json.table_reports.concat(records);
+        window.sessionStorage.setItem('db', JSON.stringify(db_s_json));
+      }
+    }
+    else if (table_name == 'table_elements') {
+      if (type_of_storage == 'in-memory') {
+        let existing_records = this.select(table_name, type_of_storage);
+        this.db_m.table_elements = existing_records.concat(records);
+      }
+      else {
+        let db_s_json = this.read_session_storage();
+        db_s_json.table_elements = db_s_json.table_elements.concat(records);
+        window.sessionStorage.setItem('db', JSON.stringify(db_s_json));
+      }
+    }
+  }
+
+  update(table_name, topic, new_data) {
+    const type_of_storage = this.get_type_of_storage(topic);
+
+    if (table_name == 'table_reports') {
+      console.error('Update table_reports not supported');
+    }
+    else if (table_name == 'table_elements') {
+      if (type_of_storage == 'in-memory')
+        this.db_m.table_elements = new_data;
+      else {
+        let db_s_json = this.read_session_storage();
+        db_s_json.table_elements = new_data;
+        window.sessionStorage.setItem('db', JSON.stringify(db_s_json));
+      }
+    }
   }
 }
+
+let db2 = new DB();
 
 function DB_SELECT_EMULATION_check_if_report_ctag_and_topic_match_them_in_user_filters(r, user_filters) {
   debug_helper(arguments, DEBUG);
@@ -69,9 +144,11 @@ function DB_SELECT_all_WHERE_user_filters(user_filters) {
   // AND date_time BETWEEN (user_filters.date_time_from, user_filters.date_time_to)
   debug_helper(arguments, DEBUG);
 
+  const table_reports = db2.select('table_reports', user_filters.topic);
+
   let found_reports = [];
-  for (let i in db) {
-    let report = db[i];
+  for (let i in table_reports) {
+    let report = table_reports[i];
     if (DB_SELECT_EMULATION_check_if_report_ctag_topic_dates_match_user_filters(report, user_filters))
       found_reports.push(report);
   }
@@ -83,10 +160,22 @@ function DB_SELECT_DISTINCT_topics_AND_elements_WHERE_ctag_topic(user_filters) {
   // AND ctag = user_filters.ctag
   // AND topic = user_filters.topic
   debug_helper(arguments, DEBUG);
+
   const ctag = user_filters['ctag'];
   const topic = user_filters['topic'];
-  console.log(db_elements);
-  let found_elements = db_elements[ctag][topic];
-  let found_topics = Object.keys(db_elements[ctag]);
+
+  const table_elements = db2.select('table_elements', topic);
+
+  let found_elements = [];
+  let found_topics = [];
+
+  if (table_elements) {
+    let topics = table_elements[ctag];
+    if (topics) {
+      found_elements = table_elements[ctag][topic];
+      found_topics = Object.keys(table_elements[ctag]);  
+    }
+  }
+
   return {'topics': found_topics, 'elements': found_elements};
 }
--- src/rx_api/dtu_rx_api.js
+++ src/rx_api/dtu_rx_api.js
@@ -8,33 +8,40 @@ function DB_INSERT_report(r) {
   if (r.element_path[0] !== '')
     r.element_path.unshift(''); // add to the beginning as "all" elements for filter
 
-  db.push(r);
+  db2.insert([r], 'table_reports', r.topic);
+  //db.push(r);
 }
 
 function DB_UPDATE_elements(r) {
   debug_helper(arguments, DEBUG);
+
   const element = r['element'];
   const ctag = r['ctag'];
   const topic = r['topic'];
 
-  if (!db_elements[ctag]) {
-    db_elements[ctag] = {};
-    db_elements[ctag][topic] = [element];
+  const table_elements = db2.select('table_elements', topic);
+
+  if (!table_elements[ctag]) {
+    table_elements[ctag] = {};
+    table_elements[ctag][topic] = [element];
   }
   else {
-    if (!db_elements[ctag][topic]) {
-      db_elements[ctag][topic] = [element];
+    if (!table_elements[ctag][topic]) {
+      table_elements[ctag][topic] = [element];
     }
     else {
-      let already_saved_elements = db_elements[ctag][topic];
+      let already_saved_elements = table_elements[ctag][topic];
       if (!already_saved_elements.includes(element))
         already_saved_elements.push(element);
     }
   }
+
+  db2.update('table_elements', topic, table_elements);
 }
 
 function RX_API_save_to_db(r) {
   debug_helper(arguments, DEBUG);
+  
   // let report = JSON.parse(r); // parse payload after receive
   let report = r; // till no real networking - no parse to save CPU time
   DB_INSERT_report(report);
