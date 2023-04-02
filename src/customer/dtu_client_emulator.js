b1d79d4a78352aa41c88ed6a554d0851a9799c15
Alexey <ihimself@gmail.com>
2023-04-03 00:27:28 +0200

    emulator now uses sdk.js

--- src/customer/dtu_client_emulator.js
+++ src/customer/dtu_client_emulator.js
@@ -1,7 +1,3 @@
-const EMULATOR_CTAG = 'DEMO MVP';
-const EMULATOR_DTU_TAG = "dtu";
-const EMULATOR_DTU_ELEMENTS_TO_TRACK = document.querySelectorAll('[data-' + EMULATOR_DTU_TAG + ']');
-
 function EMULATOR_get_random_int_between(min, max) { // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
   return Math.floor(Math.random() * (max - min) + min);
 }
@@ -13,7 +9,7 @@ function EMULATOR_get_random_item_from_number(number) {
   return Math.floor(Math.random()*number);
 }
 
-const max_number_of_days_ago = 50;
+const max_number_of_days_ago = 30;
 const max_time_ago = Date.now() - 86400 * 1000 * max_number_of_days_ago;
 var emulated_time = max_time_ago;
 
@@ -24,68 +20,63 @@ function EMULATOR_get_random_date(random_time_frame, random_time_unit) {
   return new_emulated_time;
 }
 
-const elements = [];
-for (let i in EMULATOR_DTU_ELEMENTS_TO_TRACK) {
-  const element = EMULATOR_DTU_ELEMENTS_TO_TRACK[i];
-  if (element.dataset) {
-    let dtu_value = element.dataset[EMULATOR_DTU_TAG];
-    if (dtu_value)
-      elements.push(dtu_value);
-    else {
-      if (['A', 'BUTTON'].includes(element.tagName)) {
-        elements.push(element.innerText);
-      }
-    }
-  }
-}
-
 const topics = [
   'auto-generated (lite)',
   'auto-generated (heavy)'
 ];
 
 function EMULATOR_make_report(topic, random_time_unit) {
-  let c = {};
-  c.ctag = EMULATOR_CTAG;
-  c.topic = topic;
+  let el = EMULATOR_get_random_item_from_list(dtu.elements_to_listen_to);
+  let event = SUPPORTED_INPUT_TYPES_AND_EVENTS[el.type][0];
+  let r = dtu.form_report(el, event);
+  dtu.make_report(r)
+  dtu.report.topic = topic;
   const random_time_frame = 15;
-  c.date_time = EMULATOR_get_random_date(random_time_frame, random_time_unit);
-  let el = EMULATOR_get_random_item_from_list(elements);
-  c.element = el;
-  c.element_path = ['', el];
-  c.url_domain_name = window.location.hostname;
-  c.url_path = '/app.html';
-  return c;
+  dtu.report.date_time = EMULATOR_get_random_date(random_time_frame, random_time_unit);
+  //dtu.report.element = el;
+  return dtu.report;
 }
 
 function generate_fake_data() {
   for (let i in topics) {
     const topic = topics[i];
-    if (topic == 'auto-generated (heavy)') {
-      emulated_time = max_time_ago;
-      let t = max_time_ago;
-      while (t < Date.now()) {
-        let c = EMULATOR_make_report(topic, 1000); // UNITS_NAMES_VALUES.second);
-        t = c.date_time;
-        CLIENT_SDK_EMULATOR_send_to_telemetry_api(c);
-      }
-    }
-    else if (topic == 'auto-generated (lite)') {
-      emulated_time = max_time_ago;
-      let t = max_time_ago;
-      while (t < Date.now()) {
-        let c = EMULATOR_make_report(topic, 60000); // UNITS_NAMES_VALUES.minute);
-        t = c.date_time;
-        CLIENT_SDK_EMULATOR_send_to_telemetry_api(c);
-      }
+    if (topic == 'auto-generated (heavy)')
+      random_time_unit = 1000; // UNITS_NAMES_VALUES.second;
+    else if (topic == 'auto-generated (lite)')
+      random_time_unit = 60000; // UNITS_NAMES_VALUES.minute;
+
+    emulated_time = max_time_ago;
+    let t = max_time_ago;
+    while (t < Date.now()) {
+      let c = EMULATOR_make_report(topic, random_time_unit);
+      t = c.date_time;
+      CLIENT_SDK_EMULATOR_send_to_telemetry_api(c);
     }
   }
 }
 
-if (window.location.hostname != '') {
-  generate_fake_data();
-  // ANALYTICS_PORTAL_SDK_refresh_elements_page_data_according_to_user_filters_setup(); 
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (window.location.hostname != '1') {
+    let app_content = document.getElementById('app_content'); 
+    app_content.style.display = 'none';
+    let loading_content = document.getElementById('loading');
+    loading_content.style.display = 'block';
+
+    sleep(500).then(() => {
+      generate_fake_data();
+      
+      app_content.style.display = 'block';
+      loading_content.style.display = 'none';
+
+      ANALYTICS_PORTAL_SDK_refresh_elements_page_data_according_to_user_filters_setup(); 
+    });
+  }
+});
+
 
 // SDK APP FUNCTIONS
 //const excluded_events = "mousedown mouseup mousemove mouseover mouseout mousewheel";
