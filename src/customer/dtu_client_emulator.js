8ad0c83bb9ca549d29384eeda6257bd191cb349c
Alexey <ihimself@gmail.com>
2023-04-15 22:43:01 +0200

    added uids linear emulation

--- src/customer/dtu_client_emulator.js
+++ src/customer/dtu_client_emulator.js
@@ -2,27 +2,39 @@ function EMULATOR_get_random_int_between(min, max) { // https://developer.mozill
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-function make_normal_distribution(list) {
-  let mean_i = Math.floor(list.length / 2); // works both for odd and even, floor because id's are from 0
+function make_new_list(repeats_list) {
+  let new_list = [];
+  for (let i = 0; i < repeats_list.length; i++) {
+    let number_of_repeats = repeats_list[i];
+    for (let j = 0; j < number_of_repeats; j++)
+      new_list.push(i);
+  }
+  return new_list;
+}
+
+function make_normal_distribution(list_length) {
+  let mean_i = Math.floor(list_length / 2); // works both for odd and even, floor because id's are from 0
   let standard_deviation = 4//Math.floor(list.length / 3);
 
-  let new_i = [];
-  let debug = [];
-  for (let i in list) {
-    let new_number_of_i = (2000 * Math.exp(-0.5 * Math.pow((i - mean_i) / standard_deviation, 2))) / (standard_deviation * Math.pow(2 * Math.PI, 0.5));
-    debug.push(new_number_of_i);
-    new_i.push(Math.floor(new_number_of_i));
+  let repeats_list = [];
+  //let debug = [];
+  for (let i = 0; i < list_length; i++) {
+    let number_of_repeats = (2000 * Math.exp(-0.5 * Math.pow((i - mean_i) / standard_deviation, 2))) / (standard_deviation * Math.pow(2 * Math.PI, 0.5));
+    //debug.push(number_of_repeats);
+    repeats_list.push(Math.floor(number_of_repeats));
   }
-
-  let normal_list = [];
-  for (let i in list) {
-    let list_item = list[i];
-    let number_of_repeats = new_i[i];
-    for (let j = 0; j < number_of_repeats; j++) {
-      normal_list.push(list_item);
-    }
-  }
+  const normal_list = make_new_list(repeats_list); // repeats_list.length == list_length, so no need to pass list_length
   return normal_list;
+}
+
+function make_linear_distribution(list_length) {
+  let repeats_list = [];
+  for (let i = 0; i < list_length; i++) {
+    let new_number_of_i = parseInt(i) + 1;
+    repeats_list.push(new_number_of_i);
+  }
+  const linear_list = make_new_list(repeats_list); // repeats_list.length == list_length, so no need to pass list_length
+  return linear_list;
 }
 
 function EMULATOR_get_random_item_from_list(list) { // https://stackoverflow.com/questions/5915096/get-a-random-item-from-a-javascript-array
@@ -48,20 +60,31 @@ const topics = [
   'auto-generated (heavy)'
 ];
 
-function EMULATOR_make_report(topic, random_time_unit, list) {
-  let el = EMULATOR_get_random_item_from_list(list);
-  let event = SUPPORTED_INPUT_TYPES_AND_EVENTS[el.type][0];
-  let r = dtu.process_element_event(el, event);
+function EMULATOR_make_report(topic, random_time_unit, element, uid) {
+  let event = SUPPORTED_INPUT_TYPES_AND_EVENTS[element.type][0];
+  let r = dtu.process_element_event(element, event);
   dtu.make_report(r)
   dtu.report.topic = topic;
+  dtu.report.uid = uid;
   const random_time_frame = 15;
   dtu.report.date_time = EMULATOR_get_random_date(random_time_frame, random_time_unit);
   return dtu.report;
 }
 
 function generate_fake_data() {
-  const list = [...dtu.elements_to_listen_to].sort(() => Math.random() - 0.5);
-  const normal_list = make_normal_distribution(list);
+  // sort with random within a list because elements_to_listen_to go as in the page 
+  // and if normal distribution then middle page elements will get the most usage all the time
+  // but we want random elements got more and less
+  const list_of_elements = [...dtu.elements_to_listen_to].sort(() => Math.random() - 0.5);
+  const normal_list_of_elements_ids = make_normal_distribution(list_of_elements.length);
+
+  const base_uid = parseInt(dtu.get_uid());
+  const uids_step = -10;
+  const number_of_uids = 9;
+  const list_of_uids = [];
+  for (let i = 0; i < number_of_uids; i++)
+    list_of_uids.push(base_uid + uids_step * i);
+  const linear_list_of_uid_ids = make_linear_distribution(list_of_uids.length);
 
   for (let i in topics) {
     const topic = topics[i];
@@ -73,7 +96,11 @@ function generate_fake_data() {
     emulated_time = max_time_ago;
     let t = max_time_ago;
     while (t < Date.now()) {
-      let c = EMULATOR_make_report(topic, random_time_unit, normal_list);
+      const element_id = EMULATOR_get_random_item_from_list(normal_list_of_elements_ids);
+      const element = list_of_elements[element_id];
+      const uid_id = EMULATOR_get_random_item_from_list(linear_list_of_uid_ids);
+      const uid = list_of_uids[uid_id];
+      const c = EMULATOR_make_report(topic, random_time_unit, element, uid);
       t = c.date_time;
       CLIENT_SDK_EMULATOR_send_to_telemetry_api(c);
     }
